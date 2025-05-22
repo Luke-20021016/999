012787a7b8db40a031a00cc73c8e33e2ddb35de8
Luke-20021016 <105519750@student.swin.edu.au>
2025-05-22 10:39:32 +1000

    Add files via upload

---
+++ Group-Assignment-main/settings.php
@@ -0,0 +1,7 @@
+<?php
+// settings.php - 数据库连接配置
+$host = "localhost";       // 本地服务器
+$user = "root";            // 数据库用户名（XAMPP默认是root）
+$pwd = "";                 // 密码（XAMPP 默认无密码）
+$sql_db = "web_project";   // 数据库名称（需要提前在 phpMyAdmin 创建）
+?>
